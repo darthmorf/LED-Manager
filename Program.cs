ec4b6f729b6f7c921fa41673d9868a8c698c092d
Sam <sampoirier@email.com>
2019-11-11 13:41:16 +0000

    initial tests

---
+++ Program.cs
@@ -0,0 +1,203 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.Linq;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LEDManager
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            while (true)
+            {
+                DateTime start = DateTime.Now;
+
+                Image img = CaptureWindow(User32.GetDesktopWindow());
+
+                Bitmap bmp = new Bitmap(img);
+                PictureAnalysis.GetMostUsedColor(bmp);
+
+               // var list = new Dictionary<int, int>();
+               // for (int x = 0; x < bmp.Width; x++)
+               // {
+               //     for (int y = 0; y < bmp.Height; y++)
+               //     {
+               //         int rgb = bmp.GetPixel(x, y).ToArgb();
+               //         var added = false;
+               //         for (int i = 0; i < 10; i++)
+               //         {
+               //             if (list.ContainsKey(rgb + i))
+               //             {
+               //                 list[rgb + i]++;
+               //                 added = true;
+               //                 break;
+               //             }
+               //             if (list.ContainsKey(rgb - i))
+               //             {
+               //                 list[rgb - i]++;
+               //                 added = true;
+               //                 break;
+               //             }
+               //         }
+               //         if (!added)
+               //             list.Add(rgb, 1);
+               //     }
+               // }
+               //
+               // int mostCommon = list.Values.Max();
+               // int value = list.FirstOrDefault(x => x.Value == mostCommon).Key;
+
+                DateTime color = DateTime.Now;
+
+                //Console.WriteLine($"{value} Analysis Time:  { (color - start).ToString()}");
+                Console.WriteLine($"{PictureAnalysis.MostUsedColor.ToString()} Analysis Time:  { (color - start).ToString()}");
+
+                Console.ReadLine();
+            }           
+        }
+
+        public static Image CaptureWindow(IntPtr handle)
+        {
+            // get te hDC of the target window
+            IntPtr hdcSrc = User32.GetWindowDC(handle);
+            // get the size
+            User32.RECT windowRect = new User32.RECT();
+            User32.GetWindowRect(handle, ref windowRect);
+            int width = windowRect.right - windowRect.left;
+            int height = windowRect.bottom - windowRect.top;
+            // create a device context we can copy to
+            IntPtr hdcDest = GDI32.CreateCompatibleDC(hdcSrc);
+            // create a bitmap we can copy it to,
+            // using GetDeviceCaps to get the width/height
+            IntPtr hBitmap = GDI32.CreateCompatibleBitmap(hdcSrc, width, height);
+            // select the bitmap object
+            IntPtr hOld = GDI32.SelectObject(hdcDest, hBitmap);
+            // bitblt over
+            GDI32.BitBlt(hdcDest, 0, 0, width, height, hdcSrc, 0, 0, GDI32.SRCCOPY);
+            // restore selection
+            GDI32.SelectObject(hdcDest, hOld);
+            // clean up 
+            GDI32.DeleteDC(hdcDest);
+            User32.ReleaseDC(handle, hdcSrc);
+            // get a .NET image object for it
+            Image img = Image.FromHbitmap(hBitmap);
+            // free up the Bitmap object
+            GDI32.DeleteObject(hBitmap);
+            return img;
+        }
+    }
+
+    public static class PictureAnalysis
+    {
+        public static List<Color> TenMostUsedColors { get; private set; }
+        public static List<int> TenMostUsedColorIncidences { get; private set; }
+
+        public static Color MostUsedColor { get; private set; }
+        public static int MostUsedColorIncidence { get; private set; }
+
+        private static int pixelColor;
+
+        private static Dictionary<int, int> dctColorIncidence;
+
+        public static void GetMostUsedColor(Bitmap theBitMap)
+        {
+            TenMostUsedColors = new List<Color>();
+            TenMostUsedColorIncidences = new List<int>();
+
+            MostUsedColor = Color.Empty;
+            MostUsedColorIncidence = 0;
+
+            // does using Dictionary<int,int> here
+            // really pay-off compared to using
+            // Dictionary<Color, int> ?
+
+            // would using a SortedDictionary be much slower, or ?
+
+            dctColorIncidence = new Dictionary<int, int>();
+
+            // this is what you want to speed up with unmanaged code
+            for (int row = 0; row < theBitMap.Size.Width; row++)
+            {
+                for (int col = 0; col < theBitMap.Size.Height; col++)
+                {
+                    pixelColor = theBitMap.GetPixel(row, col).ToArgb();
+
+                    if (dctColorIncidence.Keys.Contains(pixelColor))
+                    {
+                        dctColorIncidence[pixelColor]++;
+                    }
+                    else
+                    {
+                        dctColorIncidence.Add(pixelColor, 1);
+                    }
+                }
+            }
+
+            // note that there are those who argue that a
+            // .NET Generic Dictionary is never guaranteed
+            // to be sorted by methods like this
+            var dctSortedByValueHighToLow = dctColorIncidence.OrderByDescending(x => x.Value).ToDictionary(x => x.Key, x => x.Value);
+
+            // this should be replaced with some elegant Linq ?
+            foreach (KeyValuePair<int, int> kvp in dctSortedByValueHighToLow.Take(10))
+            {
+                TenMostUsedColors.Add(Color.FromArgb(kvp.Key));
+                TenMostUsedColorIncidences.Add(kvp.Value);
+            }
+
+            MostUsedColor = Color.FromArgb(dctSortedByValueHighToLow.First().Key);
+            MostUsedColorIncidence = dctSortedByValueHighToLow.First().Value;
+        }
+
+    }
+
+    public class GDI32
+    {
+
+        public const int SRCCOPY = 0x00CC0020; // BitBlt dwRop parameter
+        [DllImport("gdi32.dll")]
+        public static extern bool BitBlt(IntPtr hObject, int nXDest, int nYDest,
+            int nWidth, int nHeight, IntPtr hObjectSource,
+            int nXSrc, int nYSrc, int dwRop);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleBitmap(IntPtr hDC, int nWidth,
+            int nHeight);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr CreateCompatibleDC(IntPtr hDC);
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteDC(IntPtr hDC);
+        [DllImport("gdi32.dll")]
+        public static extern bool DeleteObject(IntPtr hObject);
+        [DllImport("gdi32.dll")]
+        public static extern IntPtr SelectObject(IntPtr hDC, IntPtr hObject);
+    }
+
+    /// <summary>
+    /// Helper class containing User32 API functions
+    /// </summary>
+    public class User32
+    {
+        [StructLayout(LayoutKind.Sequential)]
+        public struct RECT
+        {
+            public int left;
+            public int top;
+            public int right;
+            public int bottom;
+        }
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetDesktopWindow();
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowDC(IntPtr hWnd);
+        [DllImport("user32.dll")]
+        public static extern IntPtr ReleaseDC(IntPtr hWnd, IntPtr hDC);
+        [DllImport("user32.dll")]
+        public static extern IntPtr GetWindowRect(IntPtr hWnd, ref RECT rect);
+    }
+}
