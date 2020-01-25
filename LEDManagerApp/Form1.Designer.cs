bdb9b3ae7b80520f705a4cf2c3c2587fee718166
darthmorf <sampoirier@email.com>
2020-01-25 23:33:43 +0000

    Built manager app

---
+++ LEDManagerApp/Form1.Designer.cs
@@ -0,0 +1,81 @@
+﻿namespace LEDManagerApp
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            this.console = new System.Windows.Forms.RichTextBox();
+            this.trayIcon = new System.Windows.Forms.NotifyIcon(this.components);
+            this.SuspendLayout();
+            // 
+            // console
+            // 
+            this.console.AccessibleRole = System.Windows.Forms.AccessibleRole.ScrollBar;
+            this.console.BackColor = System.Drawing.SystemColors.Control;
+            this.console.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            this.console.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.console.Font = new System.Drawing.Font("Segoe UI Mono", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.console.ForeColor = System.Drawing.SystemColors.WindowText;
+            this.console.HideSelection = false;
+            this.console.Location = new System.Drawing.Point(0, 0);
+            this.console.Name = "console";
+            this.console.ReadOnly = true;
+            this.console.ScrollBars = System.Windows.Forms.RichTextBoxScrollBars.Vertical;
+            this.console.Size = new System.Drawing.Size(800, 450);
+            this.console.TabIndex = 0;
+            this.console.Text = "";
+            // 
+            // trayIcon
+            // 
+            this.trayIcon.Icon = ((System.Drawing.Icon)(resources.GetObject("trayIcon.Icon")));
+            this.trayIcon.Text = "LED Manager";
+            this.trayIcon.MouseClick += new System.Windows.Forms.MouseEventHandler(this.notifyIcon_MouseClick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.SystemColors.ControlDarkDark;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.console);
+            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
+            this.Name = "Form1";
+            this.Text = "LED Manager";
+            this.Resize += new System.EventHandler(this.Form1_Resize);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.RichTextBox console;
+        private System.Windows.Forms.NotifyIcon trayIcon;
+    }
+}
+
---
+++ LEDManagerApp/Form1.cs
@@ -0,0 +1,138 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Diagnostics;
+using System.Drawing;
+using System.IO;
+using System.Linq;
+using System.Net.Sockets;
+using System.Runtime.InteropServices;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace LEDManagerApp
+{
+    public partial class Form1 : Form
+    {
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AllocConsole();
+
+        [DllImport("kernel32.dll")]
+        public static extern Boolean FreeConsole();
+
+        [DllImport("kernel32.dll")]
+        public static extern Boolean AttachConsole(Int32 ProcessId);
+        public Form1()
+        {
+            InitializeComponent();
+            Hide();
+            trayIcon.Visible = true;
+
+            PerformanceCounter cpuCounter;
+            PerformanceCounter ramCounter;
+            cpuCounter = new PerformanceCounter("Processor", "% Processor Time", "_Total");
+            ramCounter = new PerformanceCounter("Memory", "% Committed Bytes In Use");
+            GPUMonitor gpuCounter = new GPUMonitor();
+
+            //Color screenColor;
+            Bitmap image;
+            float cpuValue;
+            float ramValue;
+            float gpuValue;
+
+            TcpClient client = new TcpClient();
+            console.Text = console.Text + "Connecting.....\n";
+            client.Connect(serverIp, serverPort);
+
+            Stream stream = client.GetStream();
+
+            int delay = 250;
+            gpuCounter.InitGpuInfo();
+
+            while (true)
+            {
+
+                cpuValue = cpuCounter.NextValue();
+                ramValue = ramCounter.NextValue();
+                gpuValue = gpuCounter.GetGpuInfo();
+
+                console.Text = console.Text + $"{cpuValue}% {ramValue}% {gpuValue}%\n";
+                console.Select(console.Text.Length - 1, 0);
+
+                StatusPacket.SendPacket(stream, cpuValue, ramValue, gpuValue);
+
+                Thread.Sleep(delay);
+                Application.DoEvents();
+            }
+        }
+
+        public const bool doImage = false;
+        public const string serverIp = "129.11.102.134";
+        public const int serverPort = 2610;
+
+        private void Form1_Resize(object sender, EventArgs e)
+        {
+            //if the form is minimized  
+            //hide it from the task bar  
+            //and show the system tray icon (represented by the NotifyIcon control)  
+            if (this.WindowState == FormWindowState.Minimized)
+            {
+                Hide();
+                trayIcon.Visible = true;
+            }
+        }
+
+        private void notifyIcon_MouseClick(object sender, MouseEventArgs e)
+        {
+            Show();
+            this.WindowState = FormWindowState.Normal;
+            trayIcon.Visible = false;
+        }
+
+    }
+
+    public static class StatusPacket
+    {
+        struct Data
+        {
+            public float cpuUsage;
+            public float ramUsage;
+            public float gpuUsage;
+        }
+
+        public static void SendPacket(Stream stream, float cpuUsage, float ramUsage, float gpuUsage)
+        {
+            Data data = new Data();
+            //data.image = image;
+            data.cpuUsage = cpuUsage;
+            data.ramUsage = ramUsage;
+            data.gpuUsage = gpuUsage;
+
+
+            int size = Marshal.SizeOf(data);
+            byte[] dataBytes = new byte[size];
+
+            IntPtr ptr = Marshal.AllocHGlobal(size);
+            Marshal.StructureToPtr(data, ptr, true);
+            Marshal.Copy(ptr, dataBytes, 0, size);
+            Marshal.FreeHGlobal(ptr);
+
+            SendBytes(stream, dataBytes);
+        }
+
+        public static void SendBytes(Stream stream, byte[] bytes)
+        {
+            if (stream.CanWrite)
+            {
+                stream.Write(bytes, 0, bytes.Length);
+            }
+            else
+            {
+                Console.WriteLine("Could not write data to stream as it has closed!");
+            }
+        }
+    }
+}
