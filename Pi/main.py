96d863679533aba2482ac6f4d2e823ce7edd21a8
darthmorf <sampoirier@email.com>
2020-05-10 22:49:23 +0100

    Shared python debug + LED drawing library

--- Pi/main.py
+++ Pi/main.py
@@ -1,174 +1,94 @@
-import socket
-import struct
-import pygame
-import sys
-import io
-from PIL import Image
-import numpy as np
-
 class Color:
-  def __init__(self, a, r, g, b):
-    self.a = a
+  def __init__(self, r, g, b):
     self.r = r
     self.g = g
     self.b = b
 
   def toString(self):
-    return "(" + str(self.a) + ", " + str(self.r) + ", " + str(self.g) + ", " + str(self.b) + ")"
+    return "(" + str(self.r) + ", " + str(self.g) + ", " + str(self.b) + ")"
 
   def toRGB(self):
-    return (self.r,self.g,self.b)
-
-
-
-class StatusPacket:
-  def __init__(self, data):
-    data = struct.unpack("fff", data)
-    self.cpuUsage = round(data[0])
-    self.ramUsage = round(data[1])
-    self.gpuUsage = round(data[2])
-
-  def toString(self):
-    return "ImageStatus: " + str(self.cpuUsage) + "% " + str(self.ramUsage) + "% " + str(self.gpuUsage) + "%"
-
-
+    return (self.r, self.g, self.b)
 
 class Grid:
-  def __init__(self):
-    self.width = 32
+  def __init__(self, debug=False):
+    self.debug = debug
+    self.width = 64
     self.height = 32
-    defaultColor = Color(255,255,255,255)
-    self.contents = []
-    self.barHeight = 22;
-    self.cpuHistory = []
-    self.ramHistory = []
-    self.gpuHistory = []
+    defaultColor = Color(0,0,0)
+    self.values = []
 
-    for x in range(self.width):
+    for y in range(self.height):
       row = []
-      for y in range(self.height):
+      for x in range(self.width):
         row.append(defaultColor)
-      self.contents.append(row)
+      self.values.append(row)
+
+    if self.debug:
+      import pygame
+      self.pygame = pygame
+      self.pygame.init()
+      self.pixelModifier = 8
+      self.surface = pygame.display.set_mode((self.width*self.pixelModifier + self.width, self.height*self.pixelModifier + self.height))
+      self.pygame.display.set_caption("LED Matrix Simulator")
+    else:
+      from rgbmatrix import RGBMatrix, RGBMatrixOptions
+      options = RGBMatrixOptions()
+      options.rows = self.height
+      options.cols = self.width
+      options.parallel = 1
+      options.hardware_mapping = 'adafruit-hat'
+
+      self.matrix = RGBMatrix(options = options)
+      self.canvas = self.matrix.CreateFrameCanvas()
 
   def setBG(self, color):
-    for i in range(len(self.contents)):
-      for j in range(len(self.contents[i])):
-        self.contents[i][j] = color
+    for y in range(self.height):
+      for x in range(self.width):
+        self.values[y][x] = color
 
-  def drawOutlines(self):
-    for x in range(len(self.contents)):
-      for y in range(len(self.contents[x])):
-        shouldSet = False
-        if y == 4 or y == 27:
-          if (x > 3 and x < 10) or (x > 12 and x < 19) or (x > 21 and x < 28):
-            shouldSet = True
-        elif y > 4 and y < 27:
-          if x == 4 or x == 9 or x == 13 or x == 18 or x == 22 or x == 27:
-            shouldSet = True
+  def setPixel(self, x, y, color):
+    if x > self.width:
+      print(x + " is out of bounds.")
+      return
+    if y > self.height:
+      print(y + " is out of bounds.")
+      return
 
-        if shouldSet:
-           self.contents[y][x] = Color(255, 255, 255, 255)
-
-  def drawBars(self, cpuValue, ramValue, gpuValue):
-    self.drawBar(cpuValue, self.cpuHistory, 5)
-    self.drawBar(ramValue, self.ramHistory, 14)
-    self.drawBar(gpuValue, self.gpuHistory, 23)
-
-  def drawBar(self, value, history, xStart):
-    history.append(value)
-    if len(history) > 4:
-      del history[0]
-
-    i = 0
-    value = 0
-    for value_ in history:
-      value += value_
-      i += 1
-
-    value = int(value/i)
-
-    yValue = int((value / 100) * self.barHeight)
-
-    if yValue < 1:
-      yValue = 1
-
-    for i in range(yValue):
-      for j in range(4):
-        colorValue = int((i / self.barHeight) * 255)
-        self.contents[26-i][xStart+j] = Color(255, colorValue, 255-colorValue, 0)
-
-  def drawOutline(self, color):
-    for x in range(len(self.contents)):
-      for y in range(len(self.contents[x])):
-        if y == 0 or y == 1 or y == 30 or y == 31 or x == 0 or x == 1 or x == 30 or x == 31:
-          self.contents[y][x] = color
-
-
-class GridDisplay:
-  def __init__(self, grid):
-    self.grid = grid
-    pygame.init()
-    self.pixelModifier = 8
-    self.surface = pygame.display.set_mode((grid.width*self.pixelModifier + grid.width, grid.height*self.pixelModifier + grid.height))
-    pygame.display.set_caption("LED Matrix Simulator")
+    self.values[y][x] = color
 
   def update(self):
-    pygame.event.get()
-    self.surface.fill((100,0,100))
-    x = 0
-    y = 0
-
-    for i in range(len(self.grid.contents)):
-      for j in range(len(self.grid.contents[i])):
-        pygame.draw.rect(self.surface, self.grid.contents[i][j].toRGB(), pygame.Rect(x, y, self.pixelModifier, self.pixelModifier))
-        x += self.pixelModifier + 1
+    if self.debug:
+      self.pygame.event.get()
+      self.surface.fill((255,0,255))
       x = 0
-      y += self.pixelModifier + 1
-    
-    pygame.display.flip()
+      y = 0
+
+      for i in range(self.height):
+        for j in range(self.width):
+          self.pygame.draw.rect(self.surface, self.values[i][j].toRGB(), self.pygame.Rect(x, y, self.pixelModifier, self.pixelModifier))
+          x += self.pixelModifier + 1
+        x = 0
+        y += self.pixelModifier + 1
+      
+      self.pygame.display.flip()
+    else:
+      self.canvas.Clear()
+      for i in range(self.height):
+        for j in range(self.width):
+          self.canvas.SetPixel(j, i, self.values[i][j].r, self.values[i][j].g, self.values[i][j].b)
+
+      self.canvas = self.matrix.SwapOnVSync(self.canvas)
 
 
 def __main__():
-  HOST = "129.11.102.134"
-  PORT = 2610
-  socketSize = 2048
 
-  grid = Grid()
-  gridDisplay = GridDisplay(grid)
+  grid = Grid(True)
+  grid.setBG(Color(0,0,255))
+  grid.setPixel(10, 10, Color(255,0,0))
 
-  
-  with socket.socket(socket.AF_INET, socket.SOCK_STREAM) as sock:
-    sock.bind((HOST, PORT))
-    
-    while True:
-      grid.setBG(Color(255, 0, 0, 0))   
-      gridDisplay.update()
-      sock.listen()
-      print("Waiting for Connection...")
-      conn, addr = sock.accept()
-
-      dataPacket = None
-      with conn:
-        conn_ = conn
-        print('Connected by', addr)
-        while True:
-          try:     
-            packet = conn.recv(socketSize)
-            
-            if str(packet) == "b''":
-              break
-            dataPacket = StatusPacket(packet)
-            print(dataPacket.toString())
-          
-            grid.setBG(Color(255, 0, 0, 0))
-            grid.drawBars(dataPacket.cpuUsage, dataPacket.ramUsage, dataPacket.gpuUsage)   
-            grid.drawOutlines()      
-            gridDisplay.update()
-
-
-          except Exception as e: print(e)
-    
-
+  while True:
+    grid.update()
 
 __main__()
  
